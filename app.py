9022c02b2087c76db03e708801ad0e40f85cd289
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-06-04 12:38:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Face Mask Detection AI Project"
+    return "Face Mask Detection AI Project Running Successfully"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
