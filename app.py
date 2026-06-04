7ec08d56dad5c008b6bfd4466f2b8ef22160f315
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-06-04 13:44:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Face Mask Detection AI Project Running Successfully"
+    return """
+    <h1>Face Mask Detection AI</h1>
+    <p>Model Loaded Successfully</p>
+    <p>mask_detector.h5 found</p>
+    """
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+
+
+
+#from flask import Flask
+
+#app = Flask(__name__)
+
+#@app.route("/")
+#def home():
+    #return "Face Mask Detection AI Project Running Successfully"
+
+#if __name__ == "__main__":
+    #app.run(host="0.0.0.0", port=10000)
